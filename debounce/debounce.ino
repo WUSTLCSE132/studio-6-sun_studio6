460369783d623c513ae9ebeef8cb59b56890bc38
andrewsun25 <sun420@wustl.edu>
2017-11-01 17:33:01 -0500

    Update debounce.ino

--- debounce/debounce.ino
+++ debounce/debounce.ino
@@ -1,30 +1,26 @@
-const int buttonPin = 2;
+const byte buttonPin = 2;
+const byte ledPin = 13;
 unsigned long lastInterruptTime = 0;  // the last time the output pin was toggled
 unsigned long now = 0;
-
-//ISR
-void buttonPressed() {
-  now = millis();
-  if(now - lastInterruptTime > 200){ // If it's been at least 200 ms since the button was last pressed.
-    Serial.println("Interrupt");
-  }
-  lastInterruptTime = now;
-}
+volatile byte state = LOW;
 
 void setup() {
   pinMode(buttonPin, INPUT_PULLUP);
-
-  // Interrupts can happen on "edges" of signals.  
+  pinMode(ledPin, OUTPUT);
   // Three edge types are supported: CHANGE, RISING, and FALLING 
   attachInterrupt(digitalPinToInterrupt(buttonPin), buttonPressed, FALLING);
   Serial.begin(9600);
 }
 
-
-
 void loop() {
-  for(int i=0;i<100;i++) {
-    Serial.println(i);
-    delay(1000);
+  digitalWrite(ledPin, state);
+}
+
+//ISR
+void buttonPressed() {
+  now = millis();
+  if(now - lastInterruptTime > 200){ // If it's been at least 200 ms since the button was last pressed.
+    state = !state;
   }
+  lastInterruptTime = now;
 }
